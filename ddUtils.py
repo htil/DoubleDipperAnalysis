674bfbfa2d75e9ee2fc239144aa9d48734319d90
Ethan Mines <elmines325@gmail.com>
2019-11-25 18:09:24 -0600

    Create an epoch of practice data

--- ddUtils.py
+++ ddUtils.py
@@ -1,27 +1,72 @@
 import numpy as np
+import mne
 
+import sys
+from functools import reduce
 from typing import Iterable, Sequence, Generator
 
-def contigEpochs(entries: Sequence[object]):
+def contigEpochs(entries: Sequence[object], epochSeconds: int = None, verbose=True):
+    eeg = extract(lambda entry: entry["type"] == "eeg", entries)
+    eeg = flattenSignal(eeg, "eeg")
+    eeg = list(eeg)
+
+    rawInfo = eeg[0]["info"]
+    samplingRate = rawInfo["samplingRate"]
+    channels = rawInfo["channelNames"]
+    numChannels = len(channels)
+
+    info = mne.create_info(channels,samplingRate,ch_types=["eeg"]*numChannels)
+
+
     epochBounds = extract(lambda ent: ent["type"] in {"fixationPoint", "end"}, entries)
     epochBounds = map(lambda ent: ent["timestamp"], epochBounds)
     epochBounds = list(epochBounds)
-
-    print("epoch lenghts in time:")
-    i = 0
-    while i < len(epochBounds) - 1:
-        print( epochBounds[i+1] - epochBounds[i] )
-        i += 1
-
-    #eeg = extract(lambda entry: entry["type"] == "eeg", entries)
-    #eeg = flattenSignal(eeg, "eeg")
-    #eeg = list(eeg)
-    #eeg = np.array(eeg, dtype=np.float32, copy=False)
-
-    #info = eeg[0]["info"]
-    #samplingRate = info["samplingRate"]
+    numEpochs = len(epochBounds) - 1
 
 
+    if not epochSeconds:
+        n = min(50, len(epochBounds) - 1)
+        avg = 0.
+        for i in range(n):
+            avg += (epochBounds[i+1] - epochBounds[i]) / n
+        epochSeconds = int(avg / 1e3)
+    epochSamples = epochSeconds * samplingRate;
+
+
+    if verbose: sys.stderr.write(f"Obtaining {numEpochs} epochs\n");
+    data = []
+    j = 0
+    for i in range(numEpochs):
+        [start,end] = epochBounds[i:i+2]
+        j = findNext(lambda samp: samp["timestamp"] >= start, eeg, j)
+        if j < 0: break
+
+        count = 0
+        sample = eeg[j]
+        while count < epochSamples and sample["timestamp"] <= end:
+            data.append(sample["data"])
+            count += 1
+            j += 1
+            if j >= len(eeg): break
+            sample = eeg[j]
+        missing = epochSamples - count
+        if missing: data.extend( [data[-1] for _ in range(missing)] ) #Pad with the last value
+    eeg = None #Free by dereferencing
+
+    data = np.array(data)
+    data /= 1e6 # Convert from microvolts to volts
+    data = np.reshape(data, [numEpochs, numChannels, epochSamples])
+
+
+    epochs = mne.EpochsArray(data, info)
+    return epochs
+
+
+
+def findNext(pred, seq, start=0):
+    for i in range(start, len(seq)):
+        if pred(seq[i]): return i
+    return -1
 
 def flattenSignal(data: Iterable[object], key) -> Generator[object,None,None]:
     if callable(key): fn = key
